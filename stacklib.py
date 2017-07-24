19cd90154905c131cb51d9953d5a77de1fdfbacf
frodo821 <1234567890.sakai.jp@gmail.com>
2017-07-25 02:17:32 +0900

    Add files via upload

---
+++ stacklib.py
@@ -0,0 +1,71 @@
+#-*-coding:utf-8;-*-
+
+class Stack:
+    def __init__(self, T):
+        self.__stack = list()
+        self.TYPE = T
+    def __add__(self, other):
+        if type(other) is self.TYPE:
+            self.PUSH(other)
+            return self
+        elif type(elem) is not self.TYPE:
+            raise TypeError("Item being not initial type cannot be pushed into stack.")
+        elif self.GetType() is not elem.GetType():
+            raise TypeError("Item being not initial type cannot be pushed into stack.")
+        elif type(other) is type(self):
+            co = other.Copy()
+            while co.HasItem():
+                self.PUSH(co.POP())
+            return self
+        else:
+            raise TypeError("Don't support between those types.")
+    def __eq__(self, other):
+        if type(self) is not type(other):
+            return False
+        cs = self.Copy()
+        co = other.Copy()
+        if not cs.GetItemCount() == co.GetItemCount():
+            return False
+        while cs.HasItem():
+            if not cs.POP() == co.POP():
+                return False
+        return True
+    def __ne__(self, other):
+        return not self == other
+    def __iadd__(self, elem):
+        if type(elem) is self.TYPE:
+            self.PUSH(elem)
+            return self
+        elif type(elem) is not self.TYPE:
+            raise TypeError("Item being not initial type cannot be pushed into stack.")
+        elif self.GetType() is not elem.GetType():
+            raise TypeError("Item being not initial type cannot be pushed into stack.")
+        elif type(elem) is type(self):
+            ce = elem.Copy()
+            while ce.HasItem():
+                self.PUSH(ce.POP())
+            return self
+        else:
+            raise TypeError("Don't support between those types.")
+    def PUSH(self, item):
+        if type(item) is self.TYPE:
+            self.__stack.append(item)
+        else:
+            raise TypeError("Item being not initial type cannot be pushed into stack.")
+    def POP(self):
+        if self.HasItem():
+            return self.__stack.pop()
+        raise ReferenceError("Cannot pop from this stack, because this has no item.")
+    def ROLL(self):
+        self.__stack.insert(0, self.__stack.pop)
+    def Copy(self):
+        cp = Stack(self.TYPE)
+        cp.__stack = list(self.__stack)
+        return cp
+    def HasItem(self):
+        return not len(self.__stack) == 0
+    def GetType(self):
+        return self.TYPE
+    def GetItemCount(self):
+        return len(self.__stack)
+
